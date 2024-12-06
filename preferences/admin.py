5c0c76f25917046bbacaead49061e3ff551d7403
UsithaDJay <usitha.20@cse.mrt.ac.lk>
2024-12-06 10:33:28 +0530

    Add Admin Integration

--- preferences/admin.py
+++ preferences/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import AccountSettings, NotificationSettings, ThemeSettings, PrivacySettings
 
-# Register your models here.
+@admin.register(AccountSettings)
+class AccountSettingsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'username', 'email')
+
+@admin.register(NotificationSettings)
+class NotificationSettingsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'frequency', 'email_notifications', 'push_notifications')
+
+@admin.register(ThemeSettings)
+class ThemeSettingsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'theme', 'font_size')
+
+@admin.register(PrivacySettings)
+class PrivacySettingsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'profile_visibility', 'data_sharing')
