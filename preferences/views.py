cb23d4528e3df83e6c634da12278af418e52e929
UsithaDJay <usitha.20@cse.mrt.ac.lk>
2024-12-06 11:08:21 +0530

    Add API views for retrieving and updating user preferences

--- preferences/views.py
+++ preferences/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import AccountSettings, NotificationSettings, ThemeSettings, PrivacySettings
+from .serializers import (
+    AccountSettingsSerializer,
+    NotificationSettingsSerializer,
+    ThemeSettingsSerializer,
+    PrivacySettingsSerializer,
+)
 
-# Create your views here.
+
+class AccountSettingsView(generics.RetrieveUpdateAPIView):
+    queryset = AccountSettings.objects.all()
+    serializer_class = AccountSettingsSerializer
+
+
+class NotificationSettingsView(generics.RetrieveUpdateAPIView):
+    queryset = NotificationSettings.objects.all()
+    serializer_class = NotificationSettingsSerializer
+
+
+class ThemeSettingsView(generics.RetrieveUpdateAPIView):
+    queryset = ThemeSettings.objects.all()
+    serializer_class = ThemeSettingsSerializer
+
+
+class PrivacySettingsView(generics.RetrieveUpdateAPIView):
+    queryset = PrivacySettings.objects.all()
+    serializer_class = PrivacySettingsSerializer
