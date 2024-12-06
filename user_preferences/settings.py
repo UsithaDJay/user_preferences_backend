7b539f6aa2ef0f33978d7393cccb839088fdf44d
UsithaDJay <usitha.20@cse.mrt.ac.lk>
2024-12-06 10:55:11 +0530

    Install Django REST Framework and configure settings

--- user_preferences/settings.py
+++ user_preferences/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "preferences"
+    "preferences",
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
